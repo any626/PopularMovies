754dc347b4e983b6ad38e10b2d0bf3ad37299fd7
Alejandro <aleee626@gmail.com>
2015-07-22 16:00:31 -0700

    Start of API extraction

--- app/src/main/java/com/example/alejandro/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/alejandro/popularmovies/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.example.alejandro.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 
 /**
@@ -19,5 +24,33 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, String[]>{
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            if(params.length == 0){
+                return null;
+            }
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonString = null;
+
+            try{
+
+            } catch (IOException e){
+
+            } finally{
+
+            }
+
+        }
     }
 }
